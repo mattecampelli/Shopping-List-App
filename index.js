ad67ff810cc6e69344cbc8d503aaf01f0fecf25c
mattecampelli <mattecampelli@gmail.com>
2026-08-13 15:30:35 +0200

    from firebase database to localstorage

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js"
-import { getDatabase,
-         ref,
-         push,
-         onValue,
-         remove } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-database.js"
-const firebaseConfig = {
-    databaseURL: "https://shopping-list-app-fb3db-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js"
+// import { getDatabase,
+//          ref,
+//          push,
+//          onValue,
+//          remove } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-database.js"
+// const firebaseConfig = {
+//     databaseURL: "https://shopping-list-app-fb3db-default-rtdb.europe-west1.firebasedatabase.app/"
+// }
 
-const app = initializeApp(firebaseConfig)
-const database = getDatabase(app)
-const referenceInDB = ref(database, "items")
+// const app = initializeApp(firebaseConfig)
+// const database = getDatabase(app)
+// const referenceInDB = ref(database, "items")
 
 const addBtn = document.getElementById("add-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 const inputEl = document.getElementById("input-el")
+let items = []
+let itemsFromLocalStorage = JSON.parse(localStorage.getItem("items"))
+
+if (itemsFromLocalStorage) {
+    items = itemsFromLocalStorage
+    render(items)
+}
 
 function render(items) {
     let listItems = ""
@@ -27,21 +34,27 @@ function render(items) {
     ulEl.innerHTML = listItems
 }
 
-onValue(referenceInDB, function(snapshot) {
-    const snapshotDoesExist = snapshot.exists()
-    if (snapshotDoesExist) {
-        const snapshotValues = snapshot.val()
-        const items = Object.values(snapshotValues)
-        render(items)
-    }
-})
+// onValue(referenceInDB, function(snapshot) {
+//     const snapshotDoesExist = snapshot.exists()
+//     if (snapshotDoesExist) {
+//         const snapshotValues = snapshot.val()
+//         const items = Object.values(snapshotValues)
+//         render(items)
+//     }
+// })
 
 addBtn.addEventListener("click", function() {
-    push(referenceInDB, inputEl.value)
+    // push(referenceInDB, inputEl.value)
+    items.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("items", JSON.stringify(items))
+    render(items)
 })
 
 deleteBtn.addEventListener("click", function() {
-    remove(referenceInDB)
+    // remove(referenceInDB)
     ulEl.innerHTML = ""
+    localStorage.clear()
+    items = []
+    render(items)
 })
